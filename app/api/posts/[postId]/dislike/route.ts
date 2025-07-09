c16e5a0a40a8cc1b0eed829d4a37b2b43401e116
Aloknath-sah <kumaraditya6396@gmail.com>
2025-07-09 21:17:11 +0530

    deployment fix

--- app/api/posts/[postId]/dislike/route.ts
+++ app/api/posts/[postId]/dislike/route.ts
@@ -3,15 +3,22 @@ import { Post } from "@/models/post.model";
 import { NextRequest, NextResponse } from "next/server";
 
 //post dislikes
-export const POST = async (
-  req: NextRequest,
-  context: { params: { postId: string } }
-) => {
-  const postId = context.params.postId;
+export const POST = async (req: NextRequest) => {
   try {
     await connectDB();
+
+    const url = new URL(req.url);
+    const postId = url.pathname.split("/").pop();
+
+    if (!postId) {
+      return NextResponse.json(
+        { error: "No post ID provided" },
+        { status: 400 }
+      );
+    }
+
     const userId = await req.json();
-    const post = await Post.findById({ _id: postId });
+    const post = await Post.findById(postId);
     if (!post) return NextResponse.json({ error: "Post not found" });
     await post.updateOne({ $pull: { likes: userId } });
     return NextResponse.json({ message: "Post disliked successfully" });
